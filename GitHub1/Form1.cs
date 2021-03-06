0142733a86de6ff1996e27847a5e6b5f63b3343e
LyckyLITH <79251656+LyckyLITH@users.noreply.github.com>
2021-03-06 11:57:13 +0100

    Add files via upload

---
+++ GitHub1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitHub1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            double vysledek = a + b;
+            label1.Text = "Výsledek je " + vysledek;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            double vysledek = a * b;
+            label2.Text = "Výsledek je " + vysledek;
+        }
+    }
+}
